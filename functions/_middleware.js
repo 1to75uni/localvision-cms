85e54b26dd3319c088e1b4ffe4fa7085cbcd783b
1to75uni <1to75uni@users.noreply.github.com>
2026-05-07 12:24:42 +0900

    Deploy LocalVision CMS v1.7.2 auto app id final

--- functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-// LocalVision CMS v1.6.6-simple-login
-// 운영 방침: 첫 화면 로그인은 프론트/CMS 진입용으로만 사용합니다.
-// API는 TV 앱/Player/현장 운영 안정성을 위해 서버단에서 막지 않습니다.
-// 즉, /api/login은 비밀번호 확인용이고, 나머지 API는 인증 쿠키 없이도 접근 가능합니다.
-export async function onRequest(context) {
-  return context.next()
-}
--- functions/api/logout.js
+++
@@ -1,5 +0,0 @@
-import { clearAdminCookie, authJson } from '../_lib/auth.js'
-
-export async function onRequestPost() {
-  return authJson({ ok: true }, 200, { 'set-cookie': clearAdminCookie() })
-}
